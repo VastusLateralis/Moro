2fdb7f7fc81abcb0d7c26bc743b33c7c52b8b508
Thomas Johannessen <thomasjohannessen@thomass-macbook-pro.local>
2018-07-31 23:21:05 +0200

    First upload

--- Moro/ViewController.swift
+++ Moro/ViewController.swift
@@ -14,7 +14,19 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
 
+    @IBOutlet weak var my_Text: UITextField!
+    
+    @IBAction func my_btn(_ sender: UIButton) {
+     my_Text.text = "Thomas";
+     my_Text.textAlignment = .center;
+     my_Text.textColor = UIColor.cyan;
+     my_Text.font = UIFont.boldSystemFont(ofSize: 12)
+     my_Text.font = UIFont.systemFont(ofSize: 12)
+     my_Text.font = UIFont(name: "Arial", size: 14)
+ }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
